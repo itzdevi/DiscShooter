8d15a688801d047489460f1b4e81b776645df864
nanonoam <97607867+nanonoam@users.noreply.github.com>
2023-11-11 09:27:23 +0200

    chassis

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,13 +1,38 @@
 package frc.robot;
 
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+
+// Class holding all the constants
 public final class Constants {
-  public static final double CYCLE_DT = 0.02;
+  public static final double CYCLE_DT = 0.02;  // Cycle DT constant
 
+  // Nested class for Shooter constants
   public static class ShooterConstants {
-    public static final int FLYWHEEL_MOTOR_ID = 1;
-    public static final String LIMELIGHT_TABLE_KEY = "limelight-iii";
+    public static final int FLYWHEEL_MOTOR_ID = 1;  // ID of the flywheel motor
+    public static final String LIMELIGHT_TABLE_KEY = "limelight-iii";  // Key for limelight table
 
-    public static final double FLYWHEEL_VELOCITY_Multiplayer = 0.1;
-    public static final int SERVO_ID = 9;
+    public static final double FLYWHEEL_VELOCITY_Multiplayer = 0.1;  // Velocity multiplier for the flywheel
+
+    public static final int SERVO_ID = 9;  // ID of the Servo
+
+    public static final int GYRO_ID = 14;  // ID of the Gyro
+    // IDs of front and back motors for left and right
+    public static final int LeftFrontMotor = 2;
+    public static final int LeftBackMotor = 3;
+    public static final int RightFrontMotor = 4;
+    public static final int RightBackMotor = 5;
+
+    // Constants to set left and right inverted
+    public static final boolean LeftInverted = true;
+    public static final boolean RightInverted = false;
+
+    // Constant for Wheel Circumference, Gear Ratio, Pulses per meter and rotation
+    public static final double WheelCircumference = 4 * 0.0254 * Math.PI; // 4 Inch wheels
+    public static final double GearRatio = 8.14;
+    public static final double PulsePerRotation = 2048;
+    public static final double PULSES_PER_METER = (1/WheelCircumference)*GearRatio*PulsePerRotation;
+
+    public static DifferentialDriveKinematics KINEMATICS = new DifferentialDriveKinematics(1);
   }
+
 }
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -5,41 +5,67 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.sensors.PigeonIMU;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
 import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 
 import static frc.robot.Constants.ShooterConstants.*;
 
 public class Shooter extends SubsystemBase {
-  private final TalonFX mFlywheel;
-  private final Servo servo;
+  private TalonFX mFlywheel;
+  private Servo servo;
 
-  private final NetworkTable limelightTable;
+  private TalonFX left;
+  private TalonFX right;
 
-
+  private PigeonIMU gyro;
+  private Field2d field;
+  DifferentialDriveOdometry odometry;
+  private DifferentialDriveWheelSpeeds wheelSpeeds;
   // Constructor initializes subsystem components
   public Shooter() {
     super();
     mFlywheel = new TalonFX(FLYWHEEL_MOTOR_ID);
     servo = new Servo(SERVO_ID);
 
-    limelightTable = NetworkTableInstance.getDefault().getTable(LIMELIGHT_TABLE_KEY);
+    left = initMotors(LeftFrontMotor, LeftBackMotor, LeftInverted);
+    right = initMotors(RightFrontMotor, RightBackMotor, RightInverted);
 
-    setFlywheelPID(0.7, 0, 0);
+    setPID(mFlywheel, 0.7, 0, 0);
     mFlywheel.setInverted(false);
+
+    gyro = new PigeonIMU(GYRO_ID);
+    field = new Field2d();
+    odometry = new DifferentialDriveOdometry(Rotation2d.fromDegrees(0), 0, 0);
     
   }
 
-  // Methods to get Limelight data
-  public double getTargetX() {
-    return limelightTable.getEntry("tx").getDouble(0.0);
+  @Override
+  public void periodic() {
+    odometry.update(Rotation2d.fromDegrees(gyro.getFusedHeading()),
+    left.getSelectedSensorPosition()/PULSES_PER_METER,
+    right.getSelectedSensorPosition()/PULSES_PER_METER);
+    field.setRobotPose(odometry.getPoseMeters());
   }
+
+  private TalonFX initMotors(int main, int follower, boolean invert) {
+    TalonFX m = new TalonFX(main);
+    TalonFX f = new TalonFX(follower);
+    m.setInverted(invert);
+    f.setInverted(invert);
+    f.follow(m);
+    setPID(m, 0.2, 0.03,0.003);
+    return m;
+}
   
-  public void setFlywheelPID(double kP, double kI, double kD) {
+  public void setPID(TalonFX motor, double kP, double kI, double kD) {
     mFlywheel.config_kP(0, kP);
     mFlywheel.config_kP(0, kI);
     mFlywheel.config_kP(0, kD);
@@ -61,6 +87,12 @@ public class Shooter extends SubsystemBase {
   }
   public void setServoToAngle(double servoAngle){
     servo.setAngle(servoAngle);
+  }
+
+  public void setVelocity(ChassisSpeeds chassisSpeeds){
+    wheelSpeeds = KINEMATICS.toWheelSpeeds(chassisSpeeds);
+    left.set(ControlMode.Velocity, wheelSpeeds.leftMetersPerSecond*PULSES_PER_METER);
+    right.set(ControlMode.Velocity, wheelSpeeds.rightMetersPerSecond*PULSES_PER_METER);
   }
   
   
